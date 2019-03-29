61428593b2c21aad62401e27275c820403fac80a
Richard <44544144+RichardSabo@users.noreply.github.com>
2019-03-29 17:33:16 +0200

    dfdf

--- OdnoNebo/js/control.js
+++ OdnoNebo/js/control.js
@@ -852,51 +852,14 @@ var ymap = function() {
 }
  
 
-
-var x = 0, y = 0,
-    vx = 0, vy = 0,
-	ax = 0, ay = 0;
-	
-var sphere = document.querySelector(".s2_cloud_1");
- 
-if (window.DeviceMotionEvent != undefined) {
-	window.ondevicemotion = function(e) {
-		ax = event.accelerationIncludingGravity.x * 5;
-		ay = event.accelerationIncludingGravity.y * 5;
-	}
- 
-	setInterval( function() {
-		var landscapeOrientation = window.innerWidth/window.innerHeight < 1;
-		if ( landscapeOrientation) {
-			vx = vx + ay;
-			vy = vy + ax;
-		} else {
-			vy = vy - ay;
-			vx = vx + ax;
-		}
-		vx = vx * 0.98;
-		vy = vy * 0.98;
-		y = parseInt(y + vy / 50);
-		x = parseInt(x + vx / 50);
-		
-		boundingBoxCheck();
-		
-		sphere.style.top = y + "px";
-		sphere.style.left = x + "px";
-		
-	}, 25);
-} 
- 
- 
-function boundingBoxCheck(){
-	if (x<0) { x = 0; vx = -vx; }
-	if (y<0) { y = 0; vy = -vy; }
-	if (x>document.documentElement.clientWidth-20) { x = document.documentElement.clientWidth-20; vx = -vx; }
-	if (y>document.documentElement.clientHeight-20) { y = document.documentElement.clientHeight-20; vy = -vy; }
-	
-}
-
-
+$('.s2_cloud_1').parallaxify();
+$('.s2_cloud_2').parallaxify();
+$('.s2_cloud_3').parallaxify();
+$('.s2_cloud_4').parallaxify();
+$('.s2_cloud_5').parallaxify();
+$('.s2_cloud_6').parallaxify();
+$('.s2_cloud_7').parallaxify();
+$('.s2_cloud_8').parallaxify();
 
 
 
