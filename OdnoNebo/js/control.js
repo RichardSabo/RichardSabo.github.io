48c401ae38b45fc114cfeb79fad29605b938ffe4
Richard <44544144+RichardSabo@users.noreply.github.com>
2019-03-29 18:04:39 +0200

    Update control.js

--- OdnoNebo/js/control.js
+++ OdnoNebo/js/control.js
@@ -868,9 +868,7 @@ $.parallaxify.positionProperty.mytransform= {
        }
    };
         
- $('#cloud1').parallaxify({
-        positionProperty: 'mytransform'
-    });
+ $('#cloud1').parallaxify();
 $('#cloud2').parallaxify();
 $('.s2_cloud_3').parallaxify();
 $('.s2_cloud_4').parallaxify();
@@ -879,6 +877,51 @@ $('.s2_cloud_6').parallaxify();
 $('.s2_cloud_7').parallaxify();
 $('.s2_cloud_8').parallaxify();
 
+    
+      $.parallaxify({
+        // enable parallax effect for horizontal, vertical or both directions
+        horizontalParallax: true,
+        verticalParallax: false,
+
+        // enable or disable parallax effect for elements or backgrounds
+        parallaxBackgrounds: true,
+        parallaxElements: true,
+
+        // set which positioning property is to be used
+        // options include 'position' or 'transform' using css transformations
+        positionProperty: 'position',
+
+        // enable for responsive layouts
+        // (upon orientation changes or window resizing element positions are reevaluated
+        responsive: false,
+
+        // enable or disable mouse or gyroscope data as input for the plugin
+        useMouseMove: true,
+        useGyroscope: true,
+
+        // use a Low Pass Filter to smooth sensor readings (1 = no filter)
+        alphaFilter: 0.9,
+
+        // set which motion type algorithm is to be used
+        // options include 'natural', 'linear', 'gaussian', or 'performance'
+        motionType: 'natural',
+        mouseMotionType: 'gaussian',
+
+        // define which sensor input has priority over the other
+        // options are either 'mouse' or 'gyroscope'
+        inputPriority: 'mouse',
+
+        // define the delta angle (0 < motionAngle < 90) 
+        // that is used to render max parallax in this direction
+        motionAngleX: 80,
+        motionAngleY: 80,
+
+        // enable of adjustment of base position (using a Low Pass Filter)
+        // (adapting to device usage while plugin is running)
+        adjustBasePosition: true,
+        // alpha for Low Pass Filter used to adjust average position
+        alphaPosition: 0.05,
+    });
 
 
 //   // Bind an event to window.orientationchange that, when the device is turned,
